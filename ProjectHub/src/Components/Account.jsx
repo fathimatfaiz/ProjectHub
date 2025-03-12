54b8c95582eaf9555c264f0192ad5ff8f899e703
fathimatfaiz <thahirfathima253@gmail.com>
2025-03-12 09:48:51 +0530

    updated

--- ProjectHub/src/Components/Account.jsx
+++ ProjectHub/src/Components/Account.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 const Account = () => {
+  const {id} = useParams()
+  useEffect(() => {
+    axios.get('http://localhost:3000/auth/student/'+id)
+    .then(result => console.log(result.data))
+    .catch(err => console.log(err))
+  }, [])
   return (
     <div>Account</div>
   )
